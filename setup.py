8e9216b08733b4e1eaff5a39e19d3aa03cd220c2
sarambl <s.m.blichner@geo.uio.no>
2020-01-28 16:56:38 +0100

    first

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='AR6_CH6_RCMIPFIGS',
+    version='v00',
+    packages=['ar6_ch6_rcmipfigs', 'ar6_ch6_rcmipfigs.data_in', 'ar6_ch6_rcmipfigs.notebooks',
+              'ar6_ch6_rcmipfigs.data_postproc'],
+    url='',
+    license='',
+    author='sarambl',
+    author_email='',
+    description=''
+)
